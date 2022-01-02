013d9e44d9dfaab6721c4a514f54fdfd8e256ec3
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-01-02 12:23:50 +0800

    用于测试functions.c的存放测试用的main函数的文件

--- swb/test.c
+++ swb/test.c
@@ -1,20 +1,47 @@
 # include<stdio.h>
 # include<malloc.h>
 # include<string.h>
+// #include "stdlib.h"
+# include "functions.c"
 
-int main(){
-    char * str = (char*)malloc(sizeof(char)*20);
-    for(int i=0 ; i<3;i++){
-        if(i==2){
-            scanf("%s",str);
-        }else
-        scanf("%[^,]%*c",str);
-        printf("\n%s",str);
-    }
-    return 0;
+// int main(){
+//     char * str = (char*)malloc(sizeof(char)*20);
+//     for(int i=0 ; i<3;i++){
+//         if(i==2){
+//             scanf("%s",str);
+//         }else
+//         scanf("%[^,]%*c",str);
+//         printf("\n%s",str);
+//     }
+//     system("pause");
+//     return 0;
+// }
+
+
+//测试functions.c用的main函数
+int main()
+{
+	char tempC ;
+	char * inputCSVfile = (char*)malloc(200*sizeof(char));
+ 	printf("输入存储邻接矩阵的CSV文件的地址：\n>");
+	// while((tempC = getchar()) != '\n' && tempC != EOF);           //清空stdin的输入缓存,如果前方没有输入加该句反而会导致意外停止
+	fgets(inputCSVfile,200 , stdin);   //输入的文件路径长度限为200个字符，超过就会报错（linux环境下gcc不支持gets）
+	// fputs(inputCSVfile,stdout);     //输出写法
+	if(inputCSVfile[strlen(inputCSVfile) - 1] == '\n') //fgets取得的字符串末尾会含'\n',导致该路径字符串不可用，因此需要将其最后一个字符换为'\0'
+        inputCSVfile[strlen(inputCSVfile) - 1] = '\0' ;//
+	CreateUDN(inputCSVfile);
+	CreateUDN("./inputExamples/matrix.CSV");
+
+	system("pause");
+	return 0;
 }
 
+// // int main(){
+// //     printf("hello world\n");
+// //     printf("hello world\n");
+// //     return 0;
 
+// // }
 
 
 
