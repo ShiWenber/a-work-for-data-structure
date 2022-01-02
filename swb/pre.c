be83d07b69dc660a3aa838da93e7fa184ed8cba9
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-01-02 12:22:36 +0800

    将所有全局变量，宏还有函数预定义全部转移到pre.c

--- swb/pre.c
+++ swb/pre.c
@@ -17,7 +17,19 @@ typedef struct
     int vexnum, arcnum;     /* 顶点数，边数 */
 } MGraph;                   /* 定义图的类型 */
 MGraph G;                   /* 把图定义为全局变量 */
-VertexType Building[NUM];           /*建筑*/
+VertexType Building[NUM+1];           /*建筑,编号从1开始，留着哨兵位置方便search,其中Building[0].int存的是地点总数*/
 int P[NUM][NUM];            /* */
 long int D[NUM];            /* 辅助变量存储最短路径长度 */
 int x[NUM] = {0};              //原框架为int x[9] = {0}; 
+
+int CreateUDN(char * inputCSVfile);//void CreateUDN(int v, int a);        /* ---造图函数 */
+void narrate();                      /*---说明函数*/
+void ShortestPath(int num);          /*最短路径函数*/
+void output(int sight1, int sight2); /*输出函数*/
+char Menu();                         /*---主菜单 */
+char InitiMenu();                  /*---初始化子菜单*/
+void search();                       /*---查询建筑信息 */
+char SearchMenu();                   /* 查询子菜单 */
+void HaMiTonian(int input);                /* 哈密尔顿图的遍历 */
+void NextValue(int input);
+void display(); /* 显示遍历结果 */
